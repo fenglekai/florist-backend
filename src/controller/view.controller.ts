74d1c9d3d66f5598f079abc45049f4d7344c5f25
fenglekai <674640130@qq.com>
2023-12-04 15:55:43 +0800

    feat: 使用nunjucks模板渲染

--- src/controller/view.controller.ts
+++ src/controller/view.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Get, Inject } from '@midwayjs/core';
 import { Context } from '@midwayjs/koa';
 
-@Controller('/')
+@Controller('/view', { ignoreGlobalPrefix: true })
 export class HomeController {
   @Inject()
   ctx: Context;
 
-  @Get('/', { ignoreGlobalPrefix: true })
-  async home() {
-    await this.ctx.render('index.nj', { user: 'midway' });
+  @Get('/public')
+  async render() {
+    await this.ctx.render('publicFile.nj', { user: 'midway' });
   }
 }

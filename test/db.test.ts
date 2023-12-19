ab8b75b9eba601c501ef2b7400e3fb5d10013a45
fenglekai <674640130@qq.com>
2023-12-19 17:07:57 +0800

    fix(test): 解决测试用例问题: pnpm改用npm

--- test/db.test.ts
+++ test/db.test.ts
@@ -10,108 +10,12 @@ describe.only('test/db.test.ts', () => {
     const service = await app
       .getApplicationContext()
       .getAsync<GoodsService>(GoodsService);
-    const result = await service.add({
-      name: 'string',
-      description: 'string',
-      price: 0,
-      like_num: 0,
-      cate: 1,
-      static_table: [1],
-    });
-    console.log(result);
+    const result = await service.find();
 
     // use expect by jest
-    expect(result).toReturn();
+    expect(result).not.toHaveLength(0);
 
     // close app
     await close(app);
   });
 });
-
-// describe('GoodsService', async () => {
-//   const app = await createApp<Framework>();
-//   let goodsService = await app
-//   .getApplicationContext()
-//   .getAsync<GoodsService>(GoodsService);
-
-//  it('should call save method', async () => {
-//    const goodsDTO = {
-//      name: 'test',
-//      description: 'test',
-//      price: 1,
-//      like_num: 1,
-//      cate: 1,
-//      static_table: [1, 2, 3],
-//    };
-//    const result = await goodsService.save(goodsDTO);
-//    expect(result).toBeDefined();
-//  });
-
-//  it('should call find method', async () => {
-//    const result = await goodsService.find();
-//    expect(result).toBeDefined();
-//  });
-
-//  it('should call update method', async () => {
-//    const goodsDTO = {
-//      name: 'test',
-//      description: 'test',
-//      price: 1,
-//      like_num: 1,
-//      cate: 1,
-//      static_table: [1, 2, 3],
-//    };
-//    const id = 1;
-//    const result = await goodsService.update(goodsDTO, id);
-//    expect(result).toBeDefined();
-//  });
-
-//  it('should call delete method', async () => {
-//    const id = 1;
-//    const result = await goodsService.delete(id);
-//    expect(result).toBeDefined();
-//  });
-
-//  it('should call restore method', async () => {
-//    const id = 1;
-//    const result = await goodsService.restore(id);
-//    expect(result).toBeDefined();
-//  });
-
-//  it('should call remove method', async () => {
-//    const id = 1;
-//    const result = await goodsService.remove(id);
-//    expect(result).toBeDefined();
-//  });
-
-//  it('should call batchAdd method', async () => {
-//    const goodsDTOs = [
-//      {
-//        name: 'test',
-//        description: 'test',
-//        price: 1,
-//        like_num: 1,
-//        cate: 1,
-//        static_table: [1, 2, 3],
-//      },
-//    ];
-//    const result = await goodsService.batchAdd(goodsDTOs);
-//    expect(result).toBeDefined();
-//  });
-
-//  it('should call batchUpdate method', async () => {
-//    const goodsDTOs = [
-//      {
-//        id: 1,
-//        name: 'test',
-//        description: 'test',
-//        price: 1,
-//        like_num: 1,
-//        cate: 1,
-//        static_table: [1, 2, 3],
-//      },
-//    ];
-//    const result = await goodsService.batchUpdate(goodsDTOs);
-//    expect(result).toBeDefined();
-//  });
-// });
